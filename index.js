f763bff0b552f745076aabd7c7b03b6838551772
KOUSHIK DEB <koushikofficial1113@gmail.com>
2026-07-03 14:30:08 +0530

    Update MySQL connection password

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const mysql = require('mysql');
 var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'ishu'
+    password: '#Koushik460'
 });
 
 connection.connect(function (err) {
